4e1bdaed49767df39591135baca3d86ccc2e267e
Willian Gonçalves <willian.p.g@outlook.com>
2024-01-25 13:41:08 -0300

    Requisito 01 - Corrigindo erros

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,7 +1,8 @@
 from src.pre_built.counter import count_ocurrences
 
+
 def test_counter():
     path = "data/jobs.csv"
     word = "Python"
-    assert count_ocurrences(path, word.lower) == 1639
-    assert count_ocurrences(path, word.upper) == 1639
+    assert count_ocurrences(path, word.lower()) == 1639
+    assert count_ocurrences(path, word.upper()) == 1639
